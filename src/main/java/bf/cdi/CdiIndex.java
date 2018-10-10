f98ffa963c1a7b93e53401751d8a2a5651389ca5
harp07 <harp07@mail.ru>
2018-10-10 17:05:32 +0400

    10-10-18_17-00 = next

--- src/main/java/bf/cdi/CdiIndex.java
+++ src/main/java/bf/cdi/CdiIndex.java
@@ -19,53 +19,58 @@ public class CdiIndex {
     }
 
     private String hashText;
-    private Map<String, String> hashes = new HashMap<>();
+    //private Map<String, String> hashes = new HashMap<>();
     private List<HashDataModel> hashesList = new ArrayList<>();
     private HashDataModel hdm;
+    
+    private String[] TYPES = {
+        "md2",
+        "md5",
+        "sha1",
+        "sha256",
+        "sha384",
+        "sha512",
+        "crc32",
+        "crc32c",
+        "adler32"
+    };
 
     @PostConstruct
     public void afterBirn() {
-        hashes.clear();
         hashesList.clear();
     }
 
+    public void calcTextHashesAll() {
+        if (!hashText.isEmpty()) {
+            hashesList.clear();
+            for (String tip: TYPES) {
+                hdm=new HashDataModel();
+                hdm.setType(tip);
+                switch(tip) {
+                    case "md2": hdm.setHash(DigestUtils.md2Hex(hashText)); break;
+                    case "md5": hdm.setHash(DigestUtils.md5Hex(hashText)); break;
+                    case "sha1": hdm.setHash(DigestUtils.sha1Hex(hashText)); break;
+                    case "sha256": hdm.setHash(DigestUtils.sha256Hex(hashText)); break;
+                    case "sha384": hdm.setHash(DigestUtils.sha384Hex(hashText)); break;
+                    case "sha512": hdm.setHash(DigestUtils.sha512Hex(hashText)); break;
+                    case "crc32": hdm.setHash(Hashing.crc32().hashUnencodedChars(hashText).toString()); break;
+                    case "crc32c": hdm.setHash(Hashing.crc32c().hashUnencodedChars(hashText).toString()); break;
+                    case "adler32": hdm.setHash(Hashing.adler32().hashUnencodedChars(hashText).toString()); break;
+                }
+                hashesList.add(hdm);
+            }
+            /*hashes.put("md2", DigestUtils.md2Hex(hashText));
+            //////////  Hashing.crc32().hashString(buf, Charsets.UTF_8).toString()
+            hashes.put("adler32",Hashing.adler32().hashUnencodedChars(hashText).toString());*/
+        }
+    }
+    
     public String getHashText() {
         return hashText;
     }
 
     public void setHashText(String hashText) {
         this.hashText = hashText;
-    }
-
-    public Map<String, String> getHashes() {
-        return hashes;
-    }
-
-    public void calcTextHashesAll() {
-        if (!hashText.isEmpty()) {
-            hashes.clear();
-            hashesList.clear();
-            hdm=new HashDataModel();
-            hdm.setType("md2");
-            hdm.setHash(DigestUtils.md2Hex(hashText));
-            hashesList.add(hdm);
-            hdm=new HashDataModel();
-            hdm.setType("md5");
-            hdm.setHash(DigestUtils.md5Hex(hashText));
-            hashesList.add(hdm);            
-            hashes.put("md2", DigestUtils.md2Hex(hashText));
-            hashes.put("md5", DigestUtils.md5Hex(hashText));
-            hashes.put("sha1", DigestUtils.sha1Hex(hashText));
-            hashes.put("sha256", DigestUtils.sha256Hex(hashText));
-            hashes.put("sha384", DigestUtils.sha384Hex(hashText));
-            hashes.put("sha512", DigestUtils.sha512Hex(hashText));
-            //////////  Hashing.crc32().hashString(buf, Charsets.UTF_8).toString()
-            hashes.put("crc32",  Hashing.crc32().hashUnencodedChars(hashText).toString());
-            hashes.put("crc32c", Hashing.crc32c().hashUnencodedChars(hashText).toString());
-            hashes.put("adler32",Hashing.adler32().hashUnencodedChars(hashText).toString());
-
-        }
-
     }
 
     public List<HashDataModel> getHashesList() {
