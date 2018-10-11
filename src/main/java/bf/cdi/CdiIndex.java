a6095c2247c654f384089bcf5551d629192448a7
harp07 <harp07@mail.ru>
2018-10-11 14:23:52 +0400

    11-10-18_14-22 = file upload changes

--- src/main/java/bf/cdi/CdiIndex.java
+++ src/main/java/bf/cdi/CdiIndex.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.apache.commons.codec.digest.DigestUtils;
