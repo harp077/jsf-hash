9b921539672f6393866574a6173031afd09668bf
harp07 <harp07@mail.ru>
2018-10-11 11:18:07 +0400

    11-10-18_11-11 = bugs fixed

--- src/main/java/bf/cdi/CdiIndex.java
+++ src/main/java/bf/cdi/CdiIndex.java
@@ -3,22 +3,17 @@ package bf.cdi;
 import bf.model.HashDataModel;
 import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.primefaces.model.UploadedFile;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Named
@@ -37,6 +32,7 @@ public class CdiIndex {
     private List<HashDataModel> fileHashesList = new ArrayList<>();
     private HashDataModel hdm;
     private UploadedFile file;
+    private File targetFile;
     
     private String[] TYPES = {
         "md2",
@@ -96,33 +92,40 @@ public class CdiIndex {
     //@Async
     public void upload() {
         if (file.getSize()<=0L) {
-            cdiMess.addMessage("Emrty file !","null-file",FacesMessage.SEVERITY_INFO);
+            cdiMess.addMessage("Empty file !","null-file",FacesMessage.SEVERITY_INFO);
             return;
         }
-        try (InputStream is = file.getInputstream()){
+        try {
             fileHashesList.clear();
+            targetFile = new File("/tmp/targetFile.tmp");
+            FileUtils.copyInputStreamToFile(file.getInputstream(), targetFile);            
             for (String tip: TYPES) {
                 hdm=new HashDataModel();
                 hdm.setType(tip);
                 switch(tip) {
-                    case "md2": hdm.setHash(DigestUtils.md2Hex(is)); break;
-                    case "md5": hdm.setHash(DigestUtils.md5Hex(is)); break;
-                    case "sha1": hdm.setHash(DigestUtils.sha1Hex(is)); break;
-                    case "sha256": hdm.setHash(DigestUtils.sha256Hex(is)); break;
-                    case "sha384": hdm.setHash(DigestUtils.sha384Hex(is)); break;
-                    case "sha512": hdm.setHash(DigestUtils.sha512Hex(is)); break;
-                    //case "crc32": hdm.setHash(Hex.encodeHexString(Hashing.crc32().hashUnencodedChars(hashText).toString().getBytes())); break;
-                    /*case "crc32": hdm.setHash(com.google.common.io.Files..asByteSource(file).hash(Hashing.crc32()).toString()); break;
-                    case "crc32c": hdm.setHash(com.google.common.io.Files.hash(file, Hashing.crc32c()).toString()); break;
-                    case "adler32": hdm.setHash(Hashing.adler32().hashString(hashText, Charsets.UTF_8).toString()); break;
-                    case "farmHashFingerprint64": hdm.setHash(Hashing.farmHashFingerprint64().hashString(hashText, Charsets.UTF_8).toString()); break;
-                    case "murmur3_128": hdm.setHash(Hashing.murmur3_128().hashString(hashText, Charsets.UTF_8).toString()); break;
-                    case "murmur3_32": hdm.setHash(Hashing.murmur3_32().hashString(hashText, Charsets.UTF_8).toString()); break;
-                    case "sipHash24": hdm.setHash(Hashing.sipHash24().hashString(hashText, Charsets.UTF_8).toString()); break;*/
+                    case "md2": hdm.setHash(DigestUtils.md2Hex(file.getInputstream())); break;
+                    case "md5": hdm.setHash(DigestUtils.md5Hex(file.getInputstream())); break;
+                    case "sha1": hdm.setHash(DigestUtils.sha1Hex(file.getInputstream())); break;
+                    case "sha256": hdm.setHash(DigestUtils.sha256Hex(file.getInputstream())); break;
+                    case "sha384": hdm.setHash(DigestUtils.sha384Hex(file.getInputstream())); break;
+                    case "sha512": hdm.setHash(DigestUtils.sha512Hex(file.getInputstream())); break;
+                    //case "crc32": hdm.setHash(com.google.common.io.Files.asByteSource(targetFile).hash(Hashing.crc32()).toString()); break;
+                    case "crc32": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.crc32()).toString()); break;
+                    case "crc32c": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.crc32c()).toString()); break;
+                    case "adler32": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.adler32()).toString()); break;
+                    case "farmHashFingerprint64": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.farmHashFingerprint64()).toString()); break;
+                    case "murmur3_128": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.murmur3_128()).toString()); break;
+                    case "murmur3_32": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.murmur3_32()).toString()); break;
+                    case "sipHash24": hdm.setHash(com.google.common.io.Files.hash(targetFile, Hashing.sipHash24()).toString()); break;
                 }
                 fileHashesList.add(hdm);
             }
-        }  catch (IOException ex) {}
+            targetFile.delete();
+        } catch (IOException ex) {
+            targetFile.delete();
+            System.out.println(ex.toString());
+            cdiMess.addMessage("IO-exception !","io-exception",FacesMessage.SEVERITY_INFO);
+        }
     }    
     
     public String getHashText() {
